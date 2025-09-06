d964ce07ebfbc70923b2984ad6be0348720d5203
anhkt015 <anhkt015@gmail.com>
2025-09-06 15:54:39 +0700

    Update ocr_engine.py

--- services/ocr_engine.py
+++ services/ocr_engine.py
@@ -7,7 +7,7 @@ def ocr_image(path):
     img = Image.open(path)
     gray = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
     thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    pytesseract.pytesseract.tesseract_cmd = "tesseract"
 
     return  pytesseract.image_to_string(img, lang='vie') 
 

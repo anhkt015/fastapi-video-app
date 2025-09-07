9b9e105be1a97b76fe0fc07f9e2498e463d1c6cc
anhkt015 <anhkt015@gmail.com>
2025-09-07 15:08:35 +0700

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from auth.jwt_handler import create_token
 
 router = APIRouter()
 
 @router.post("/login")
-def login(username: str):
-    token = create_token(username)
-    return {"access_token": token, "token_type": "bearer"}
+def login(username: str = Form(...), password: str = Form(...)):
+    # Giả sử bạn dùng tài khoản cứng để test
+    if username == "admin" and password == "123":
+        token = create_token(username)
+        return {"access_token": token, "token_type": "bearer"}
+    return {"error": "Sai thông tin đăng nhập"}
